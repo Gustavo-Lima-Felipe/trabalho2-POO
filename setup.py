2a17715149063a80e203e3a1f6381e5edb659d9b
Gustavo Lima Felipe <161981698+Gustavo-Lima-Felipe@users.noreply.github.com>
2024-12-03 14:46:10 -0300

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="NG Finances",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[],
+    description="A personal finance management package.",
+    author="Gustavo Lima Felipe",
+    license="MIT"
+)
